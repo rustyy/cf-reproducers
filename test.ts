ecde2e5d9414c40fe1e7b82c308121b93b6fe627
Felix Hofmann <felix.hofmann@rustyy.de>
2024-11-25 11:12:38 +0100

    Update test.ts
    add custom-adapter

--- test.ts
+++ test.ts
@@ -1,6 +1,7 @@
 import {HttpsProxyAgent} from "https-proxy-agent";
-import {createClient} from "contentful-management";
+import {createClient, RestAdapter} from "contentful-management";
 import axios from "axios";
+import {RestAdapterParams} from "contentful-management/dist/typings/adapters/REST/rest-adapter";
 
 console.log("PROXY_URL set:", typeof process.env.PROXY_URL !== 'undefined')
 console.log("CMA_TOKEN set:", typeof process.env.CMA_TOKEN !== 'undefined')
@@ -13,6 +14,25 @@ const spaceId = process.env.SPACE_ID!
 const environmentId = process.env.ENVIRONMENT_ID!
 const httpsAgent = new HttpsProxyAgent(proxyUrl, {keepAlive: true})
 
+class CustomCmaRestAdapter extends RestAdapter {
+    constructor(params: RestAdapterParams) {
+        super(params);
+
+        const defaultHostParameters = {
+            defaultHostname: 'api.contentful.com',
+            defaultHostnameUpload: 'upload.contentful.com',
+        };
+
+        // Although these props are marked as private, we are forced to override.
+        // Luckily, these props are not private properties in the original class.
+        // @ts-ignore
+        this.params = {
+            ...defaultHostParameters,
+            ...params,
+        };
+    }
+}
+
 /**
  * Makes an HTTP GET request to the Contentful Management API using contentful-management
  */
@@ -20,6 +40,23 @@ async function tryCmaClient() {
     const client = createClient({
         accessToken,
         httpsAgent,
+    }, {type: "plain"})
+
+    console.log(await client.environment.get({
+        spaceId,
+        environmentId
+    }))
+}
+
+/**
+ * Workaround: Makes an HTTP GET request to the Contentful Management API using contentful-management with a custom adapter
+ */
+async function tryCmaClientWithCustomAdapter() {
+    const client = createClient({
+        apiAdapter: new CustomCmaRestAdapter({
+            accessToken,
+            httpsAgent,
+        }),
     }, {type: "plain"})
 
     console.log(await client.environment.get({
@@ -44,5 +81,6 @@ async function tryRawAxios() {
 
 Promise.all([
     tryCmaClient(), // will fail
-    tryRawAxios() // works
+    tryRawAxios(), // works
+    tryCmaClientWithCustomAdapter() // now fails as well
 ]).catch(console.error)
